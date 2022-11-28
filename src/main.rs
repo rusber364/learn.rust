2467e95a0c832479e1ecfeecaa934167cb7fbfa3
Berezhnoi Ruslan <33254626+rusber364@users.noreply.github.com>
2022-11-28 20:46:40 +0000

    factorial

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
+    let num: u128 = 30;
+    let result: u128 = factorial(num);
 
+    println!("factorial of {num} is: {result}");
+}
+
+fn factorial(n: u128) -> u128 {
+    if n == 1 {
+        return 1;
+    }
+
+    n * factorial(n - 1)
 }
