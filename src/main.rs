e4617de4fa9c29f3df39faea9d0b55b2b478d9ae
Berezhnoi Ruslan <33254626+rusber364@users.noreply.github.com>
2023-01-23 10:48:27 +0000

    change ref String

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,16 @@
 fn main() {
-    let nick_name = String::from("rusber");
+    let mut nick_name = String::from("rusber");
     let nick_name_len = nick_name.len();
 
-    println!("my nickname is -> {}, len -> {nick_name_len}", nick_name);
+    println!("my nickname is -> {nick_name}, len -> {nick_name_len}");
 
-    let (update_nick_name, update_len) = add_str_to_nickname(nick_name, "364");
+    let nick_name_len = add_str_to_nickname(&mut nick_name, "364");
 
-    println!("my nickname is -> {update_nick_name}, len -> {update_len}, after update");
+    println!("my nickname is -> {nick_name}, len -> {nick_name_len}, after update");
 }
 
-fn add_str_to_nickname(mut nick_name: String, new_str: &str) -> (String, usize) {
+fn add_str_to_nickname(nick_name: &mut String, new_str: &str) -> usize {
     nick_name.push_str(new_str);
-    let nick_name_len = nick_name.len();
 
-    (nick_name, nick_name_len)
+    nick_name.len()
 }
