117a203edd5fcf2caa7dbda08b0c32a73779d2f1
Berezhnoi Ruslan <33254626+rusber364@users.noreply.github.com>
2023-01-28 15:40:52 +0000

    structs in rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
+struct User {
+    name: String,
+    age: i8,
+    developer: bool,
+}
 
+fn create_user(name: &str, age: i8, developer: bool) -> User {
+    User {
+        name: String::from(name),
+        age,
+        developer,
+    }
+}
+
+fn main() {
+    let mut user1 = create_user("rusber364", 32, true);
+
+    user1.name.push_str("__*123*_");
+
+    println!("Name -> {}", user1.name);
+    println!("Age -> {}", user1.age);
+    println!("Developer -> {}", user1.developer);
 }
