ca451eff59755d4efe8ab83d3bea5e87f2e371ec
Berezhnoi Ruslan <33254626+rusber364@users.noreply.github.com>
2023-01-31 21:03:16 +0000

    example-structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
 
+impl Rectangle {
+    fn size(&self, info: &str) -> u32 {
+        let s = self.height * self.width;
+        println!("{info}: {s}");
+
+        s
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    dbg!(rect1.size("rusber364"));
+    dbg!(rect1);
 }
